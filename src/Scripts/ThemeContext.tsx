e4f12a71311af12b419f32859802b517314727e4
Beladric <kilian.goetz@yahoo.fr>
2023-01-15 21:58:56 +0100

    Refactor eslint disable

--- src/Scripts/ThemeContext.tsx
+++ src/Scripts/ThemeContext.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function -- I'm oblige to declare an empty function to set the type
 export const COLORMODECONTEXT: React.Context<{ToggleColorMode: () => void}> =
-  React.createContext({ToggleColorMode: (): void => {}});
+  React.createContext({
+    // eslint-disable-next-line @typescript-eslint/no-empty-function -- I'm oblige to declare an empty function to set the type
+    ToggleColorMode: (): void => {},
+  });
