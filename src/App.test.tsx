deedb63e290096994dc589cc384d4751f341a3bf
Beladric <kilian.goetz@yahoo.fr>
2023-01-20 17:32:06 +0100

    Profile drawer component tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {render} from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
+describe("", function () {
+  test("renders learn react link", () => {
+    render(<App />);
+  });
 });
--- src/Components/NavbarComponent/DrawerComponents/ProfileDrawerComponent/ProfileDrawerComponent.test.tsx
+++ src/Components/NavbarComponent/DrawerComponents/ProfileDrawerComponent/ProfileDrawerComponent.test.tsx
@@ -1,24 +1,66 @@
 import React from "react";
-import {fireEvent, render, type RenderResult} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import ProfileDrawerComponent from "./ProfileDrawerComponent";
+import {createTheme, type Theme, ThemeProvider} from "@mui/material";
+import {COLORMODECONTEXT} from "../../../../Context/ThemeContext";
 
 describe("Light and dark tests", () => {
-  const [IS_OPEN_MENU, SetOpenMenu] = React.useState(false);
+  let is_open_menu: boolean = true;
+  let mode: "dark" | "light" = "dark";
 
-  const HandleOpenMenu = (): void => {
-    SetOpenMenu((is_open) => !is_open);
+  const SetMode = (new_val: Readonly<"dark" | "light">): void => {
+    mode = new_val;
   };
 
-  const {getByTestId}: RenderResult = render(
-    <ProfileDrawerComponent isOpen={IS_OPEN_MENU} Close={HandleOpenMenu} />
-  );
+  const COLORMODE: {ToggleColorMode: () => void} = {
+    ToggleColorMode: (): void => {
+      SetMode(
+        ((): "dark" | "light" => (mode === "light" ? "dark" : "light"))()
+      );
+    },
+  };
 
-  test("Change light and dark icon", () => {});
+  const THEME: Theme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
 
-  test("Change light and dark test", () => {});
+  Object.defineProperty(THEME.palette, "mode", {
+    get: () => {
+      return mode;
+    },
+  });
+
+  const Render = (): void => {
+    render(
+      <COLORMODECONTEXT.Provider value={COLORMODE}>
+        <ThemeProvider theme={THEME}>
+          <ProfileDrawerComponent
+            isOpen={is_open_menu}
+            Close={(): void => {
+              is_open_menu = !is_open_menu;
+            }}
+          />
+        </ThemeProvider>
+      </COLORMODECONTEXT.Provider>
+    );
+  };
+
+  test("If dark text work", () => {
+    Render();
+    expect(screen.getByText(/dark/i)).toBeVisible();
+  });
 
   test("If theme mode button work", () => {
-    const THEME_BUTTON: HTMLElement = getByTestId("ChangeThemeButton");
+    Render();
+    const THEME_BUTTON: HTMLElement = screen.getByTestId("ChangeThemeButton");
     fireEvent.click(THEME_BUTTON);
+    expect(THEME.palette.mode).toBe("light");
+  });
+
+  test("If light text work", () => {
+    Render();
+    expect(screen.getByText(/light/i)).toBeVisible();
   });
 });
