5b3ff1d8b79583772a4ff14da3de04dbcb4233bf
Beladric <kilian.goetz@yahoo.fr>
2023-01-24 14:21:04 +0100

    Textarea and paper

--- src/Components/MarkdownComponent/MarkdownComponent.tsx
+++ src/Components/MarkdownComponent/MarkdownComponent.tsx
@@ -1,5 +1,14 @@
 import React from "react";
-import {TextareaAutosize, type Theme, useTheme} from "@mui/material";
+import {
+  Container,
+  TextareaAutosize,
+  type Theme,
+  ToggleButton,
+  ToggleButtonGroup,
+  useTheme,
+} from "@mui/material";
+import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 export default function MarkdownComponent(): JSX.Element {
   const [MARKDOWN_INPUT, SetMarkdownInput] = React.useState("");
@@ -7,20 +16,29 @@ export default function MarkdownComponent(): JSX.Element {
   const THEME: Theme = useTheme();
 
   return (
-    <TextareaAutosize
-      autoFocus
-      style={{
-        width: "100%",
-        height: "100%",
-        boxSizing: "border-box",
-        resize: "none",
-        backgroundColor: THEME.palette.mode === "dark" ? "#1d1e20" : "white",
-        color: THEME.palette.mode === "dark" ? "white" : "black",
-        borderRadius: "0 0 10px 10px",
-      }}
-      onChange={(e): void => {
-        SetMarkdownInput(e.target.value);
-      }}
-    />
+    <Container>
+      <ToggleButtonGroup>
+        <ToggleButton value="test">
+          <FormatAlignJustifyIcon />
+          <ArrowDropDownIcon />
+        </ToggleButton>
+      </ToggleButtonGroup>
+      <TextareaAutosize
+        autoFocus
+        style={{
+          width: "100%",
+          height: "100%",
+          boxSizing: "border-box",
+          resize: "none",
+          backgroundColor: THEME.palette.mode === "dark" ? "#1d1e20" : "white",
+          color: THEME.palette.mode === "dark" ? "white" : "black",
+          borderRadius: "0 0 10px 10px",
+          fontFamily: "open sans, sans-serif",
+        }}
+        onChange={(e): void => {
+          SetMarkdownInput(e.target.value);
+        }}
+      />
+    </Container>
   );
 }
--- src/Pages/MarkdownPage/MarkdownPage.tsx
+++ src/Pages/MarkdownPage/MarkdownPage.tsx
@@ -1,14 +1,45 @@
 import React from "react";
 import MarkdownComponent from "../../Components/MarkdownComponent/MarkdownComponent";
-import {Container, Theme, useTheme} from "@mui/material";
+import {Container, Paper, Tab, Tabs} from "@mui/material";
+import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import ImageIcon from "@mui/icons-material/Image";
 
 export default function MarkdownPage(): JSX.Element {
+  const [PREVIEW_MODE, SetPreviewMode] = React.useState(0);
+
+  const HandleChangePreviewMode = (
+    event: React.SyntheticEvent,
+    new_value: number
+  ) => {
+    SetPreviewMode(new_value);
+  };
+
   return (
-    <Container
-      maxWidth="xl"
-      sx={{height: "80vh", marginBottom: "10vh", marginTop: "10vh"}}
+    <Paper
+      sx={{
+        height: "80vh",
+        margin: {lg: "10vh 10vw 10vh 10vw", md: "10vh 0 0 0"},
+        width: {lg: "80vw", md: "100vw"},
+        borderRadius: "20px",
+      }}
+      elevation={1}
     >
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Tabs value={PREVIEW_MODE} onChange={HandleChangePreviewMode}>
+          <Tab icon={<FormatAlignJustifyIcon />} label="Editor only" />
+          <Tab icon={<MenuBookIcon />} label="Editor and preview" />
+          <Tab icon={<ImageIcon />} label="Preview only" />
+        </Tabs>
+      </Container>
       <MarkdownComponent />
-    </Container>
+    </Paper>
   );
 }
